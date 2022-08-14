91f4c2891440dc8bb908c281785787d33768ae19
Eric <ericlg.link@gmail.com>
2022-08-14 09:57:57 -0700

    Added openstreetmap tiles

--- app.js
+++ app.js
@@ -6,7 +6,16 @@ const myMap = {
     //Build map
     buildMap() {
         this.map = L.map('map', {
-            
-        })
+            center: this.coordinates,
+            zoom: 12,
+        });
+
+        //add openstreetmap tiles
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            minZoom: '15',
+        }).addTo(this.map);
+
+        
     }
 }
